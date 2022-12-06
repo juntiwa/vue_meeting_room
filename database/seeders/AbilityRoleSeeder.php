6b1d76bb9beee372cd390dfe62e83c636f87fed1
juntiwa <juntima.nuch@gmail.com>
2022-12-06 13:05:39 +0700

    add abilities roles user

---
+++ database/seeders/AbilityRoleSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Ability;
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AbilityRoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $abilities = [
+            'view_any_case',
+            'booked_room_any_case',
+        ];
+
+        foreach ($abilities as $ability) {
+            Ability::query()->create(['name' => $ability]);
+        }
+
+        $roles = [
+            'admin',
+            'attendant_room',
+            'user_med',
+        ];
+
+        foreach ($roles as $role) {
+            Role::query()->create(['name' => $role]);
+        }
+
+        $admin = Role::whereName('admin')->first();
+        $admin->allowTo('view_any_case');
+
+        $attendant_room = Role::whereName('attendant_room')->first();
+        $attendant_room->allowTo('view_any_case');
+
+        $user_med = Role::whereName('user_med')->first();
+        $user_med->allowTo('view_any_case');
+
+    }
+}
---
+++ database/seeders/AddPermissionAutoSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\AddPermissionAuto;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AddPermissionAutoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            ['sap_id' => '10012341', 'role' => 'admin'],
+            ['sap_id' => '10012342', 'role' => 'attendant_room'],
+            ['sap_id' => '10012343', 'role' => 'book_period'],
+        ];
+        foreach ($users as $user){
+            AddPermissionAuto::query()->create($user);
+        }
+    }
+}
